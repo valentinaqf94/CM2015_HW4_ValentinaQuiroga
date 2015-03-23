ee53ad281a71d746f1182dad2543f4408b367d77
valentinaqf94 <v.quiroga2371@uniandes.edu.co>
2015-03-23 10:07:11 -0500

    finished

---
+++ punto3/grabar_mi_nombre.c
@@ -0,0 +1,11 @@
+//script que recibe por parametro la entrada de un archivo de audio de duracion aproximada de 3 segundos, el computador debe tener instalado el paquete sox. La duracion esta sujeta a el eco generado por el emisor de la senial. 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	char command[100];
+	system("rec -c1 -b16 minombre.wav silence -l 0 1 00:00:3.00 1\%");
+	return 0;
+
+}
